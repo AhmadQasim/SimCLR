0914c80113e9b0bdf359b3c2929bae88cb09d357
ahmad.qasim <ahmad.live.1@gmail.com>
2020-06-08 20:51:16 +0200

    further documenting

--- modules/nt_xent.py
+++ modules/nt_xent.py
@@ -3,6 +3,9 @@ import torch.nn as nn
 
 
 class NT_Xent(nn.Module):
+    """
+    The normalized temperature-scaled cross entropy loss
+    """
     def __init__(self, batch_size, temperature, device):
         super(NT_Xent, self).__init__()
         self.batch_size = batch_size
@@ -27,20 +30,37 @@ class NT_Xent(nn.Module):
         Instead, given a positive pair, similar to (Chen et al., 2017), we treat the other 2(N − 1)
         augmented examples within a minibatch as negative examples.
         """
-
+        # doc: all the comments underneath are to be considered for a batch size of 128 unless specified otherwise
         p1 = torch.cat((z_i, z_j), dim=0)
+
+        # doc: here the cosine similarity dim is 2. This works a bit differently from dimension-wise sum for example.
+        # p1.shape = [256, 1, 64] and p2.shape = [1, 256, 64], when finding cosine similarity the first two dimensions
+        # are iterated while taking the whole vector from the third dimension
         sim = self.similarity_f(p1.unsqueeze(1), p1.unsqueeze(0)) / self.temperature
 
+        # doc: suppose index for, p1 = [1, 2, 3, 4] where z_i = [1, 2] and z_j = [3, 4] and batch size = 2
+        # then the similarity matrix will look like (in terms of indexes)
+        # [11, 12, 13, 14]
+        # [21, 22, 23, 24]
+        # [31, 32, 33, 34]
+        # [41, 42, 43, 44]
+        # then torch.diag(sim, 2) = [13, 24] and torch.diag(sim, -2) = [31, 42] hence the positive samples
         sim_i_j = torch.diag(sim, self.batch_size)
         sim_j_i = torch.diag(sim, -self.batch_size)
 
+        # doc: concatenate the positive samples
         positive_samples = torch.cat((sim_i_j, sim_j_i), dim=0).reshape(
             self.batch_size * 2, 1
         )
+
+        # doc: here the self.mask filters out the main diagonals which constitute the same samples
+        # and also the minor diagonals of batch size and -batch size (look above)
         negative_samples = sim[self.mask].reshape(self.batch_size * 2, -1)
 
         labels = torch.zeros(self.batch_size * 2).to(self.device).long()
         logits = torch.cat((positive_samples, negative_samples), dim=1)
         loss = self.criterion(logits, labels)
+
+        # doc: normalize the loss i.e. 1/2N
         loss /= 2 * self.batch_size
         return loss
